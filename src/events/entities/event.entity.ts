efb205effae44d5b32661d3f62d2979f0c5fd6aa
Estelle <estelle.koji.fr>
2021-02-26 00:00:39 +0100

    adding indexes to entities

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,5 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ * To help speed up this search, we can define an index on the “name” column
+ * using the @Index decorator.
+ */
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
@@ -8,9 +13,10 @@ export class Event {
   @Column()
   type: string;
 
+  @Index()
   @Column()
   name: string;
 
-  @Column()
+  @Column('json')
   payload: Record<string, any>;
 }
